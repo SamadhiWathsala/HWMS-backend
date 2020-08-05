94b7b11b1d050837cc63b6183f7eb02c2f9eb55e
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-08-05 13:37:44 +0530

    ho data set updated

--- routes/patients.js
+++ routes/patients.js
@@ -69,7 +69,7 @@ router.post('/admit', requireLogin, verifyRole, async (req, res) => {
         res.json({ savePatient });
     }
     catch (e) {
-        res.json({ message: "erorr" })
+        res.json({ message: token })
     }
 
 
