c80df36c935601513edc353a3fc3ae36a1ec4d92
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-08-11 17:13:44 +0530

    tratment section change

--- routes/treatment.js
+++ routes/treatment.js
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
 router.get('/treats/:pID', async (req, res) => {
     try {
         const patientID = req.params.pID
-        const treatments = await (await Treatment.find(patientID)).filter(x => x.treatmentStatus === "In progress");
+        const treatments = await (await Treatment.find()).filter(x => x.treatmentStatus === "In progress" && x.patientID === patientID);
         res.json(treatments);
     }
     catch (e) {
