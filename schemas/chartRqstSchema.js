87ebec2715c72f24ae6a2357eb95eddd60e4f134
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-08-13 23:30:34 +0530

    add messurment chart request with middleware

--- schemas/chartRqstSchema.js
+++ schemas/chartRqstSchema.js
@@ -1,33 +1,33 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// module.exports = chartRqstSchema = mongoose.Schema({
-
-
-//     patientName: {
-//         type: String,
-//         required: true,
-//     },
-//     patientID: {
-//         type: String,
-//         required: true,
-//     },
-//     address: {
-//         type: String,
-//         required: true,
-//     },
-//     chartType: {
-//         type: String,
-//         required: true,
-//     },
-//     inputTime: {
-//         type: String,
-//         required: true,
-//     },
-//     description: {
-//         type: String,
-//     }
+module.exports = chartRqstSchema = mongoose.Schema({
 
 
+    patientName: {
+        type: String,
+        required: true,
+    },
+    patientID: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    chartType: {
+        type: String,
+        required: true,
+    },
+    inputTime: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    }
 
 
-// });
+
+
+});
