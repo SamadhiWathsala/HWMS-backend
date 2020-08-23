845c392536ba3972e6ca94a0827f777931243d58
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-08-23 21:04:08 +0530

    ptient masurement chart section

--- routes/chartsRqst.js
+++ routes/chartsRqst.js
@@ -19,20 +19,22 @@ const verifyRole = require("../middleware/verifyRole");
 
 //Request a new chart
 router.post('/', async (req, res) => {
+
     try {
+
         const newChartRqst = new ChartRqst({
-            patientID: req.body.patientID,
             patientName: req.body.patientName,
-            address: req.body.patientAddress,
+            patientID: req.body.patientID,
+            address: req.body.address,
             chartType: req.body.chartType,
             inputTime: req.body.inputTime,
             description: req.body.description
         });
+
         const saveChartRqst = await newChartRqst.save();
         res.json({ saveChartRqst });
     }
-    catch (e) {
-        res.json({ message: e });
+    catch (err) {
+        res.json({ message: err })
     }
-}
-);
+})
