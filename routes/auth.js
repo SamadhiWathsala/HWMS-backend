656923e6db811e0f7d3babb710e12a02a9ac465e
Samadhi Wathsala <samadhiwathsala96@gmail.com>
2020-08-26 16:40:27 +0530

    setup mail sending

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,6 @@ const { JWD_SECRET } = require('../key/keys');
 const requireLogin = require("../middleware/requireLogin");
 const verifyRole = require("../middleware/verifyRole");
 const verifyAdmin = require("../middleware/verifyAdmin");
-const { getMaxListeners } = require("../models/User");
 
 
 router.get('/protected', requireLogin, verifyRole, (req, res) => {
@@ -18,7 +17,7 @@ router.get('/protected', requireLogin, verifyRole, (req, res) => {
 
 
 //Add new user to the system
-router.post('/addUser', async (req, res) => {
+router.post('/addUser', requireLogin, verifyAdmin, async (req, res) => {
 
     try {
 
@@ -27,57 +26,53 @@ router.post('/addUser', async (req, res) => {
         const emailExist = await Users.findOne({ email: req.body.email })
         if (emailExist) return res.status(400).send('Email allready exist')
 
-        else {
-            // const newUser = new Users({
-            //     userName: req.body.userName,
-            //     stafID: req.body.staffID,
-            //     email: req.body.email,
-            //     password: bcrypt.hashSync(req.body.password, 12),
-            //     userRole: req.body.role,
-            // });
-            // console.log(req.body);
-            // const saveUser = await newUser.save();
-            // res.json({ saveUser });
+        const newUser = new Users({
+            userName: req.body.userName,
+            stafID: req.body.staffID,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 12),
+            userRole: req.body.role,
+        });
+        console.log(req.body);
+        const saveUser = await newUser.save();
+        res.json({ saveUser });
 
-            // create reusable transporter object using the default SMTP transport
-            let transporter = nodemailer.createTransport({
-                // host: "smtp.ethereal.email",
-                // port: 587,
-                // secure: false, // true for 465, false for other ports
-                service: 'gmail',
-                auth: {
-                    user: 'samadhiwathsala96@gmail.com', // generated ethereal user
-                    pass: '19@@96@@', // generated ethereal password
-                },
-            });
+        // // create reusable transporter object using the default SMTP transport
+        // let transporter = nodemailer.createTransport({
+        //     // host: "smtp.ethereal.email",
+        //     // port: 587,
+        //     // secure: false, // true for 465, false for other ports
+        //     service: 'gmail',
+        //     auth: {
+        //         user: 'samadhiwathsala96@gmail.com', // generated ethereal user
+        //         pass: 'SaM@123_', // generated ethereal password
+        //     },
+        // });
 
-            // send mail with defined transport object
-            let info = await transporter.sendMail({
-                from: '"Medi Ward Team 👻" <samadhiwathsala96@gmail.com>', // sender address
-                to: 'sanjunet10@gmail.com', // list of receivers
-                subject: `Welcome to the MEDIWARD`, // Subject line
-                text: `Hi sama,
-    
-                Congratulations !!! your account request approved by the system. Now you account is ready for you.
-                account credentials
-                staff ID : 12,
-                password : 12
-                
-                After the first login, be sure to reset your passsword as you wish...!!!` , // plain text body
-                html: `<b>Hi sama</b>,
-    
-                Congratulations !!! your account request approved by the system. Now you account is ready for you.
-                account credentials
-                staff ID : 12,
-                password : 12
-                
-                <strong>After the first login, be sure to reset your passsword as you wish...!!!</strong>`, // html body
-            });
+        // // send mail with defined transport object
+        // let info = await transporter.sendMail({
+        //     from: '"Medi Ward Team 👻" <samadhiwathsala96@gmail.com>', // sender address
+        //     to: req.body.email, // list of receivers
+        //     subject: `Welcome to the MEDIWARD`, // Subject line
+        //     text: `Hi ${req.body.userName},
 
-            console.log("Message sent: %s", info.messageId);
+        //     Congratulations !!! your account request approved by the system. Now you account is ready for you.
+        //     account credentials
+        //     staff ID : ${req.body.staffID},
+        //     password : ${req.body.staffID}
 
-        }
+        //     After the first login, be sure to reset your passsword as you wish...!!!` , // plain text body
+        //     html: `<b>Hi ${req.body.userName}</b>,
 
+        //     Congratulations !!! your account request approved by the system. Now you account is ready for you.
+        //     account credentials
+        //     staff ID : ${req.body.staffID},
+        //     password : ${req.body.staffID}
+
+        //     <strong>After the first login, be sure to reset your passsword as you wish...!!!</strong>`, // html body
+        // });
+
+        // console.log("Message sent: %s", info.messageId);
 
     }
     catch (e) {
